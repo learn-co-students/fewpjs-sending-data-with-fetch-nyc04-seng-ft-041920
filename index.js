d9b1d6cbe8da16e9067a7571cf464f225a02ccd7
senadakadric <skadric@fordham.edu>
2020-06-25 13:09:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+// import { bodyParser } from "json-server";
+
+let body = document.querySelector("body");
+
+function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      // retrieves the new ID value and appends it to the DOM
+      body.append(object.id);
+    })
+    .catch(function (error) {
+      alert("NOOOOOOOO");
+      body.append(error.message);
+    });
+}
