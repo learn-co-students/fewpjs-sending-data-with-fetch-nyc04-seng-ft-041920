e05b0ee978332d7274e4fcb6f327196667ece56e
frod25 <frod25>
2020-07-01 18:25:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({name, email})
+    })
+    .then(res => res.json())
+    .then(result => {
+        document.body.innerHTML = result["id"]
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
