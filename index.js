acf049df305c2ea17883432c7670740a70f65041
darren9897 <darrenkoomson17@gmail.com>
2020-07-02 15:20:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+let body = document.body
+
+function submitData(name, email){
+    let data = {
+        name: name,
+        email: email
+    }
+    let postRequest = {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    };
+    return fetch("http://localhost:3000/users", postRequest)
+        .then(response => response.json())
+        .then(info => body.append(info.id))
+        .catch(error => body.append(error.message))
+}
+
+
+submitData("darren", "fakedarren@gmail.com")
